6162edcd95384167103bac10048bbe07b3815be6
Yuşa <yusasarisoy@icloud.com>
2025-10-07 19:00:15 +0300

    Remove unnecessary properties from request protocol

--- Sources/CoreSession/Data/API/Request/RequestProtocol.swift
+++ Sources/CoreSession/Data/API/Request/RequestProtocol.swift
@@ -4,24 +4,12 @@ import Foundation
 
 public protocol RequestProtocol {
   associatedtype Response: Decodable
-
   var path: String { get }
   var requestType: RequestType { get }
-  var params: [String: Any] { get }
   var urlParams: [String: String?] { get }
 }
 
-// MARK: - RequestProtocol
-
 extension RequestProtocol {
-  private var host: String {
-    APIConstant.domain
-  }
-
-  var params: [String: Any] {
-    [:]
-  }
-
   public nonisolated var urlParams: [String: String?] {
     [:]
   }
